20464ba990ae1309fa41b7bb93a867395d2d4ae3
ploinarathip <ploichelsea@gmail.com>
2024-01-15 18:25:53 +0700

    Command in Eng and 'ถึง' added to Vocab

--- backend_ner/utils/model.py
+++ backend_ner/utils/model.py
@@ -10,7 +10,7 @@ class TokenClassifier:
 
         self.vocab = [
             'ศูนย์', 'หนึ่ง', 'สอง', 'สาม', 'สี่', 'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า', 'สิบ',
-            'ลบ', 'ลิงกั้ว', 'ลิงกัว', 'บัคคัล', 'บัคคอล', 'มีเสี้ยว', 'ดิสทัล', 'ทั้งหมด',
+            'ลบ', 'ลิงกั้ว', 'ลิงกัว', 'บัคคัล', 'บัคคอล', 'มีเสี้ยว', 'ดิสทัล', 'ทั้งหมด', 'ถึง',
             'มิซซิ่ง', 'คลาวน์', 'คลาวด์', 'อิมแพลนต์', 'อิมแพลนท์', 'อิมพลานต์', 'อิมพลานท์', 'บริดจ์', 'พีดีอาร์อี', 'โพลบบิ้งเดพท์', 'รีเส็ตชั่น',
             'เอ็มจีเจ', 'บรีดดิ้ง', 'บีโอพี', 'ซับปูเรชั่น', 'ซุปปูเรชั่น', 'โมบีลีตี้', 'เอ็มโอ', 'เฟอร์เคชั่น', 'ฟอร์เคชั่น', 'อันดู', 'อันโด'
         ]
@@ -19,6 +19,7 @@ class TokenClassifier:
     def inference(self, sentence):
         dp = self.maximal_matching(sentence)
         tokenized = self.backtrack(dp, sentence)
+        # tokenized = sentence.split()
         labeled_token = self.labeled_token(tokenized)
         return labeled_token
         
@@ -66,6 +67,7 @@ class TokenClassifier:
         for sow, eow in word_pos:
             tokenized.append(sentence[sow:eow+1])
         return tokenized
+    
 
     
     def labeled_token(self, tokens):
@@ -75,32 +77,35 @@ class TokenClassifier:
                 token_label_list.append([token, "Number"])
             elif token in ['ลบ']:
                 token_label_list.append([token, "Symbol"])
-            elif token in ['ลิงกั้ว', 'ลิงกัว', 'บัคคัล', 'บัคคอล', 'มีเสี้ยว', 'ดิสทัล', 'ทั้งหมด']:
+            elif token in ['ลิงกั้ว', 'ลิงกัว', 'บัคคัล', 'บัคคอล', 'มีเสี้ยว', 'ดิสทัล', 'ทั้งหมด',
+                           'Lingual', 'lingual', 'Buccal', 'buccal', 'Mesial', 'mesial', 'Distal', 'distal']:
                 token_label_list.append([token, "Side"])
-            elif token in ['มิซซิ่ง']:
+            elif token in ['มิซซิ่ง', 'Missing', 'missing']:
                 token_label_list.append([token, "Missing"])
-            elif token in ['คลาวน์', 'คลาวด์']:
+            elif token in ['คลาวน์', 'คลาวด์', 'Crown', 'crown']:
                 token_label_list.append([token, "Crown"])
-            elif token in ['อิมแพลนต์', 'อินแพลนท์', 'อิมพลานต์', 'อิมพลานท์']:
+            elif token in ['อิมแพลนต์', 'อินแพลนท์', 'อิมพลานต์', 'อิมพลานท์', 'Implant', 'implant']:
                 token_label_list.append([token, "Implant"])
-            elif token in ['บริดจ์']:
+            elif token in ['บริดจ์', 'Bridge', 'bridge']:
                 token_label_list.append([token, "Bridge"])
-            elif token in ['พีดีอาร์อี']:
+            elif token in ['พีดีอาร์อี', 'PDRE', 'pdre']:
                 token_label_list.append([token, "PDRE"])
-            elif token in ['โพลบบิ้งเดพท์']:
+            elif token in ['โพลบบิ้งเดพท์', 'ProbingDepth', 'Probingdepth', 'probingdepth']:
                 token_label_list.append([token, "PD"])
-            elif token in ['รีเส็ตชั่น']:
+            elif token in ['รีเส็ตชั่น', 'Recession', 'recession']:
                 token_label_list.append([token, "RE"])
-            elif token in ['เอ็มจีเจ']:
+            elif token in ['เอ็มจีเจ', 'MGJ', 'mgj']:
                 token_label_list.append([token, "MGJ"])
-            elif token in ['บรีดดิ้ง', 'บีโอพี']:
+            elif token in ['บรีดดิ้ง', 'บีโอพี', 'Bleeding', 'bleeding', 'BOP', 'bop']:
                 token_label_list.append([token, "BOP"])
-            elif token in ['ซับปูเรชั่น', 'ซุปปูเรชั่น']:
+            elif token in ['ซับปูเรชั่น', 'ซุปปูเรชั่น', 'Suppuration', 'suppuration']:
                 token_label_list.append([token, "SUP"])
-            elif token in ['โมบีลีตี้', 'เอ็มโอ']:
+            elif token in ['โมบีลีตี้', 'เอ็มโอ', 'Mobility', 'mobility', 'MO', 'mo']:
                 token_label_list.append([token, "MO"])
-            elif token in ['เฟอร์เคชั่น', 'ฟอร์เคชั่น']:
+            elif token in ['เฟอร์เคชั่น', 'ฟอร์เคชั่น', 'Furcation', 'furcation']:
                 token_label_list.append([token, "FUR"])
-            elif token in ['อันดู', 'อันโด']:
+            elif token in ['อันดู', 'อันโด', 'Undo', 'undo']:
                 token_label_list.append([token, "Undo"])
+            elif token in ['ถึง']:
+                token_label_list.append([token, "To"])
         return token_label_list
