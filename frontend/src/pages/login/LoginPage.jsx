cbfa14006952c9bb899d61913a48de5ce250a05e
Chayut Kritveeranant <modunkung@gmail.com>
2024-02-02 02:09:34 +0700

    fix: fix bridge

--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -10,7 +10,7 @@ import NavBar from "../../components/ui/NavBar";
 
 const LoginPage = () => {
   // [Detect between production mode or development mode]
-  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  if (!process.env.REACT_APP_NODE_ENV || process.env.REACT_APP_NODE_ENV === 'development') {
     console.log("DEV MODE", process.env.REACT_APP_BACKEND_PORT)
   } else {
     console.log("PROD MODE", process.env.REACT_APP_BACKEND_PORT)
